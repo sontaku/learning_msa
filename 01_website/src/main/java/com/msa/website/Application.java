3509a29949e945a01efe3f8cd1b4f8b0823b5772
jinyoung <jyson19@gmail.com>
2021-07-08 16:01:11 +0900

    [add] 기본 ViewController 설정

--- 01_website/src/main/java/com/msa/website/Application.java
+++ 01_website/src/main/java/com/msa/website/Application.java
@@ -1,4 +1,4 @@
-package com.example._website;
+package com.msa.website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
+
--- 01_website/src/test/java/com/msa/website/ApplicationTests.java
+++ 01_website/src/test/java/com/msa/website/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example._website;
+package com.msa.website;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
