3d21f37d637a10cd2d6ef5ddbe60ceab80e5e634
jinyoung <jyson19@gmail.com>
2021-07-08 16:52:37 +0900

    [add] init api-service example

---
+++ 03_user_api/src/test/java/com/msa/user/_user_api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa.user._user_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
